1026a88784ac3e2890a019e60a05f9547c894949
GokulHunt <gokulavarmanseshadri@gmail.com>
2021-08-01 17:38:33 +0530

    Adding new functions for removing and splitting

--- Circular_Linked_List.py
+++ Circular_Linked_List.py
@@ -69,19 +69,84 @@ class CircularLinkedList:
                 break
             ele = ele.next
         print(cllstr)
- 
- 
-# Driver program to test above function
- 
+    
+    #Function to split the circular linked list into two based on midpoint
+    def split_list(self):
+        size = len(self)
+        
+        if size == 0:
+            return None
+        elif size == 1:
+            return self.head
+        else:
+            mid = size//2
+            prev = None
+            cur = self.head
+            count = 0
+            while cur and count < mid:
+                prev = cur
+                cur = cur.next
+                count += 1
+            prev.next = self.head
+            
+            split_cllist = CircularLinkedList()
+            while cur != self.head:
+                split_cllist.append(cur.val)
+                cur = cur.next
+
+            print("Lists after splitting:")
+            self.printList()
+            split_cllist.printList()            
+        
+    #Function to remove an element from the list based on value    
+    def remove(self, val):
+        if self.head.next == self.head and self.head.val == val:
+            self.head = None        
+        else:
+            if self.head.val == val:
+                new_head = self.head.next
+                ele = self.head
+                while ele.next != self.head:
+                    ele = ele.next
+                ele.next = new_head
+                self.head = new_head
+            else:
+                ele = self.head
+                while ele.next != self.head:
+                    list_val = ele.next.val
+                    if list_val == val:
+                        ele.next = ele.next.next
+                        break
+                    ele = ele.next                                           
+
+  
 # Initialize list as empty
 cllist = CircularLinkedList()
  
 # Created linked list will be 11->2->56->12
 cllist.append(12)
 cllist.append(56)
-cllist.append(2)
+#cllist.append(2)
 cllist.prepend(11)
- 
+cllist.prepend('XD')
+cllist.prepend('Century')
+cllist.append(['a', 'b', 'c'])
+
+
+cllist.remove('Century')
+cllist.remove(56)
+cllist.remove(['a', 'b', 'c'])
+cllist.remove(12)
+cllist.remove(11)
+cllist.remove(19)
+cllist.remove('XD')
+
 print ("Contents of circular Linked List")
 cllist.printList()
+print('\n')
+
+print("Length of the list:")
 print(len(cllist))
+print('\n')
+
+cllist.split_list()
