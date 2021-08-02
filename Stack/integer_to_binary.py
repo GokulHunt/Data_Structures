3dda43338cfc6857df0a37566e4718a3a8950ca7
GokulHunt <gokulavarmanseshadri@gmail.com>
2021-08-02 20:02:26 +0530

    Adding explanation

--- Stack/integer_to_binary.py
+++ Stack/integer_to_binary.py
@@ -7,6 +7,16 @@ Created on Mon Aug  2 19:34:18 2021
 
 from stack import Stack
 
+"""
+say integer is 10
+10//2 = 5 | 10%2 == 0
+5//2 = 2  | 5%2 == 1
+2//2 = 1  | 2%2 == 0
+1//2 = 0  | 1%2 == 1
+
+bin(10) = 1010
+"""
+
 def int_to_binary(num):
     bin_stack = Stack()
     while num > 0:
